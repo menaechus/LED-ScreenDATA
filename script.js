14eec162c6c2b1fae16661cce8e2c50c1d0ab756
Menaechus <markus.tolin@gmail.com>
2023-05-08 15:06:15 +0300

    V2
    Added support for multiple led screen manufacturers. rewrote the js, mostly working correctly, sometimes json loadtimes are too slow and will generate error to console, doesnt seem to affect the site?

--- script.js
+++ script.js
@@ -1,168 +1,231 @@
-// Load the JSON file
+//a script that reads manufacturers.json and save that information to array called Manufacturers
+//then it reads the Filename data from the array and populates a object called screens with that data so that it can be referended later with manufacturer name
+//then it reads the Filaname data and populates another dropdown menu with that data
+const manufacturersFile = 'manufacturers.json';
+const dataDirectory = 'data/';
+const manufacturerDropdown = document.getElementById('manufacturer-select');
+const deviceDropdown = document.getElementById('device-select');
+var Manufacturers = [];
+var allScreens = [];
+var currentManufacturer;
+var currentDevice;
 
-fetch('ledscreens.json')
-	.then(response => response.json())
-	.then(data => {
-		const select = document.getElementById('device-select');
-		// Sort the devices by name alphabetically
-        data.sort((a, b) => a.Name.localeCompare(b.Name));
-		// Populate the dropdown menu
-		data.forEach(device => {
-			const option = document.createElement('option');
-			option.text = device.Name;
-			select.add(option);
-		});
-
-		// Update the device information when the dropdown selection changes or the number of screen pieces changes
-		function updateDeviceInfo() {
-			const deviceName = select.value;
-			const device = data.find(device => device.Name === deviceName);
-			const deviceInfoDiv = document.querySelector('.device-info');
-
-			// Clear any existing device information
-			deviceInfoDiv.innerHTML = '';
-
-			// Create a new div for the device information
-			const deviceDiv = document.createElement('div');
-			deviceDiv.classList.add('device-info');
-
-			// Calculate total pixels and total power based on the number of screen pieces
-			let numCols = document.getElementById('screen-cols').value;
-			let numRows = document.getElementById('screen-rows').value;
-            if(numCols==0){
-                numCols=1;
-            }
-            if(numRows==0){
-                numRows=1;
-            }
-			drawGrid(numRows, numCols, device);
-			const totalPixels = device.PixelWidth * device.PixelHeight;
-			const totalPower = device.PowerDraw * (numCols * numRows);
-
-			// Add each piece of device information as a new paragraph
-			Object.keys(device).forEach(key => {
-				if (key !== 'Name') {
-					const p = document.createElement('p');
-					if (key === 'TotalPixels') {
-						p.textContent = `${key}: ${totalPixels}`;
-					//} else if (key === 'PowerDraw') {
-					//	p.textContent = `${key}: ${totalPower} W`;
-					} else {
-						p.textContent = `${key}: ${device[key]}`;
-					}
-					deviceDiv.appendChild(p);
-				}
-			});
-
-			// Append the device information div to the main device information div
-			deviceInfoDiv.appendChild(deviceDiv);
-		}
-
-		function drawGrid(numRows, numCols, device) {
-			  const gridContainer = document.querySelector('.grid-container');
-			 gridContainer.innerHTML = "";// clear existing grid
-			  for (let i = 0; i < numRows; i++) {
-				const row = document.createElement("div");
-				row.classList.add("grid-row");
-				for (let j = 0; j < numCols; j++) {
-				  const column = document.createElement("div");
-				  column.classList.add("grid-column");
-				  row.appendChild(column);
-				  if ((i + j) % 2 == 0) {
-					column.style.backgroundColor = "green";
-				  } else {
-					column.style.backgroundColor = "red";
-				  }
-				}
-				gridContainer.appendChild(row);
-			  }
-			
-			const dimensions = document.querySelector(".grid-info");
-			const totalPower = device.PowerDraw * (numCols * numRows);
-			const AmountOfPhases = totalPower / 3000;
-			var AmountOfPhasesF = AmountOfPhases.toFixed(1);
-			const VerticalPixels  = device.PixelHeight * numRows; //y pixel - pysty
-			const HorizontalPixels = device.PixelWidth * numCols; //x pixel - vaaka
-			const totalAmount = numRows * numCols;const TotalPixels = VerticalPixels * HorizontalPixels;
-			const totalWidth = device.PhysicalWidth * numCols;
-			const totalHeight = device.PhysicalHeight * numRows;
-			const totalWeight = device.Weight * totalAmount;
-			var totalWeghtF = totalWeight.toFixed(2);
-			var totalHeightF = totalHeight.toFixed(2);
-			var totalWidthF = totalWidth.toFixed(2);
-			
-			dimensions.innerHTML = '';
-			dimensions.innerHTML = "Total amount: " + totalAmount; 
-			dimensions.innerHTML += "<br>Resolution: " + HorizontalPixels + "px * " + VerticalPixels + "px";
-			dimensions.innerHTML += "<br>Total Pixels: " + TotalPixels + "px";
-			dimensions.innerHTML += "<br>Physical dimensions: " + totalWidthF + "mm * " + totalHeightF + "mm";
-			dimensions.innerHTML += "<br>Total powerdraw: " + totalPower + "w";
-			dimensions.innerHTML += "<br>Number of phases (3000W): " + AmountOfPhasesF;
-			dimensions.innerHTML += "<br>Total weight: " + totalWeghtF + "kg";
-			dimensions.innerHTML += "<br><br>";
-
-		}
-
-		function callImgGen() {	
-			const gridContainer = document.querySelector('.grid-container');
-			const deviceName = select.value;
-			const device = data.find(device => device.Name === deviceName);
-			generateImage(device, gridContainer);
-		}
-
-
-		function emptyImages() {
-			const images = document.getElementById('img-holder');
-			while (images.hasChildNodes()) {
-				images.removeChild(images.firstChild);
-			}
-		}
-		
-
-		function generateImage(device, gridContainer) {
-			emptyImages();
-			const grid = document.querySelector('.grid-img-holder');
-			html2canvas((grid), { width: 1920,
-				height: 1080 }).then(function(canvas) {
-				const img = new Image();
-				img.backgroundColor = "#FFFFFF";
-				img.src = canvas.toDataURL();
-				document.getElementById('img-holder').appendChild(img);
-				document.getElementById('img-holder').children[0].style.width = "90%";
-			});
-			//if we have created an image, show save button
-			document.getElementById('img-holder').style.display = 'block';
-			document.getElementById('save-button').style.display = 'block';
-		
-		}
-
-		function clearButton() {
-			document.getElementById('img-holder').style.display = 'none';
-			emptyImages();
-		}
-
-		function saveImage() {
-			const images = document.getElementById('img-holder');
-			const image = images.children[0].src;
-			const link = document.createElement('a');
-			link.href = image;
-			link.download = 'image.png';
-			document.body.appendChild(link);
-			link.click();
-			document.body.removeChild(link);
-		}
-
-		// Update the device information when the dropdown selection changes or the number of screen pieces changes
-		select.addEventListener('change', updateDeviceInfo);
-		document.getElementById('screen-cols').addEventListener('input', updateDeviceInfo);
-		document.getElementById('screen-rows').addEventListener('input', updateDeviceInfo);
-		document.getElementById('calculate-button').addEventListener('click', callImgGen);
-		document.getElementById('save-button').addEventListener('click', saveImage);
-		document.getElementById('clear-button').addEventListener('click', clearButton);
-		//hide save button
-		document.getElementById('save-button').style.display = 'none';
-		document.getElementById('img-holder').style.display = 'none';
-
-		// Initialize the device information with the first device in the JSON file
-		updateDeviceInfo();
+function readManufacturers() {
+	$.getJSON(manufacturersFile, function(data) {
+		Manufacturers = data;
+	}).fail(function() {
+		alert('Error reading manufacturers file');
+	}).done(function() {
+		//console.log(Manufacturers);
+		populateManufacturerDropdown(Manufacturers);
 	});
+}
+		
+function populateManufacturerDropdown(Manufacturers) {
+	//sort manufacturers alphabetically
+	Manufacturers.sort(function(a, b) {
+		var nameA = a.Name.toUpperCase();
+		var nameB = b.Name.toUpperCase();
+		if (nameA < nameB) {
+			return -1;
+		}
+		if (nameA > nameB) {
+			return 1;
+		}
+		return 0;
+	});
+
+	Manufacturers.forEach(manufacturer => {
+		var option = document.createElement('option');
+			option.value = manufacturer.Name;
+			option.text = manufacturer.Name;
+			manufacturerDropdown.add(option);
+	});
+	//default dropdown to first manufacturer
+	//currentManufacturer = Manufacturers[1].Name;
+	readLedScreen(Manufacturers);
+}
+
+function readLedScreen(Manufacturers) {
+	//Read the Manufacturers.Filename to get the filename of the LED screen data json file
+	//then populate the allScreens array with the data from the json file
+	Manufacturers.forEach(manufacturer => {
+		$.getJSON(dataDirectory + manufacturer.Filename, function(data) {
+			allScreens[manufacturer.Name] = data;
+		}).fail(function() {
+			alert('Error reading LED screen file');
+		}).done(function() {
+			//console.log(allScreens);
+			populateDeviceDropdown();
+		});
+	});
+} 
+
+function populateDeviceDropdown() {
+	//populate the device dropdown with the data from the allScreens array
+	//dropdown should only be populated with the data from the selected manufacturer
+	//clear the dropdown first
+	currentManufacturer = manufacturerDropdown.value;
+	//console.log(manufacturerDropdown.value);
+	deviceDropdown.innerHTML = '';
+	var devices = allScreens[currentManufacturer];
+	//console.log(devices);
+	devices.forEach(device => {
+		var option = document.createElement('option');
+		option.value = device.Name;
+		option.text = device.Name;
+		deviceDropdown.add(option);
+	});
+	updateDeviceInfo();
+}
+
+function updateDeviceInfo() {
+	//update the device information when the dropdown selection changes or the number of screen pieces changes
+	//get the device name from the dropdown
+	//get the device information from the allScreens array
+	//calculate the total pixels and total power based on the number of screen pieces
+	//add each piece of device information as a new paragraph
+	//append the device information div to the main device information div
+	const deviceName = deviceDropdown.value;
+	const device = allScreens[currentManufacturer].find(device => device.Name === deviceName);
+	currentDevice = device;
+	const deviceInfoDiv = document.querySelector('.device-info');
+	// Clear any existing device information
+	deviceInfoDiv.innerHTML = '';
+	// Create a new div for the device information
+	const deviceDiv = document.createElement('div');
+	deviceDiv.classList.add('device-info');
+	// Calculate total pixels and total power based on the number of screen pieces
+	
+	drawGrid();
+	const totalPixels = device.PixelWidth * device.PixelHeight;
+	const totalPower = device.PowerDraw * (numCols * numRows);
+	// Add each piece of device information as a new paragraph
+	Object.keys(device).forEach(key => {
+		const p = document.createElement('p');
+		if (key === 'TotalPixels') {
+			p.textContent = `${key}: ${totalPixels}`;
+		} else if (key === 'PowerDraw') {
+			p.textContent = `${key}: ${totalPower} W`;
+		} else {
+			p.textContent = `${key}: ${device[key]}`;
+		}
+		deviceDiv.appendChild(p);
+	});
+	// Append the device information div to the main device information div
+	deviceInfoDiv.appendChild(deviceDiv);
+}
+
+function drawGrid() {
+	const deviceName = deviceDropdown.value;
+	const device = allScreens[currentManufacturer].find(device => device.Name === deviceName);
+	numCols = document.getElementById('screen-cols').value;
+	numRows = document.getElementById('screen-rows').value;
+	if(numCols==0){
+		numCols=1;
+	}
+	if(numRows==0){
+		numRows=1;
+	}
+	const gridContainer = document.querySelector('.grid-container');
+	gridContainer.innerHTML = "";// clear existing grid
+	for (let i = 0; i < numRows; i++) {
+	const row = document.createElement("div");
+	row.classList.add("grid-row");
+	for (let j = 0; j < numCols; j++) {
+		const column = document.createElement("div");
+		column.classList.add("grid-column");
+		row.appendChild(column);
+		if ((i + j) % 2 == 0) {
+		column.style.backgroundColor = "green";
+		} else {
+		column.style.backgroundColor = "red";
+		}
+	}
+	gridContainer.appendChild(row);
+	}
+			
+	const dimensions = document.querySelector(".grid-info");
+	const totalPower = device.PowerDraw * (numCols * numRows);
+	const AmountOfPhases = totalPower / 3000;
+	var AmountOfPhasesF = AmountOfPhases.toFixed(1);
+	const VerticalPixels  = device.PixelHeight * numRows; //y pixel - pysty
+	const HorizontalPixels = device.PixelWidth * numCols; //x pixel - vaaka
+	const totalAmount = numRows * numCols;const TotalPixels = VerticalPixels * HorizontalPixels;
+	const totalWidth = device.PhysicalWidth * numCols;
+	const totalHeight = device.PhysicalHeight * numRows;
+	const totalWeight = device.Weight * totalAmount;
+	var totalWeghtF = totalWeight.toFixed(2);
+	var totalHeightF = totalHeight.toFixed(2);
+	var totalWidthF = totalWidth.toFixed(2);
+			
+	dimensions.innerHTML = '';
+	dimensions.innerHTML = "Model: " + device.Name;
+	dimensions.innerHTML += "<br>Total amount: " + totalAmount; 
+	dimensions.innerHTML += "<br>Resolution: " + HorizontalPixels + "px * " + VerticalPixels + "px";
+	dimensions.innerHTML += "<br>Total Pixels: " + TotalPixels + "px";
+	dimensions.innerHTML += "<br>Physical dimensions: " + totalWidthF + "mm * " + totalHeightF + "mm";
+	dimensions.innerHTML += "<br>Total powerdraw: " + totalPower + "w";
+	dimensions.innerHTML += "<br>Number of phases (3000W): " + AmountOfPhasesF;
+	dimensions.innerHTML += "<br>Total weight: " + totalWeghtF + "kg";
+	dimensions.innerHTML += "<br><br>";
+}	
+
+function generateImage(device, gridContainer) {
+	emptyImages();
+	const grid = document.querySelector('.grid-img-holder');
+	html2canvas((grid), { width: 1920,
+		height: 1080 }).then(function(canvas) {
+		const img = new Image();
+		img.backgroundColor = "#FFFFFF";
+		img.src = canvas.toDataURL();
+		document.getElementById('img-holder').appendChild(img);
+		document.getElementById('img-holder').children[0].style.width = "90%";
+	});
+	//if we have created an image, show save button
+	document.getElementById('img-holder').style.display = 'block';
+	document.getElementById('save-button').style.display = 'block';
+
+}
+
+function callImgGen() {	
+	const gridContainer = document.querySelector('.grid-container');
+	const device = currentDevice;
+	generateImage(device, gridContainer);
+}
+
+function clearButton() {
+	document.getElementById('img-holder').style.display = 'none';
+	emptyImages();
+}
+
+function saveImage() {
+	const images = document.getElementById('img-holder');
+	const image = images.children[0].src;
+	const link = document.createElement('a');
+	link.href = image;
+	link.download = 'image.png';
+	document.body.appendChild(link);
+	link.click();
+	document.body.removeChild(link);
+}
+
+function emptyImages() {
+	const images = document.getElementById('img-holder');
+	while (images.hasChildNodes()) {
+		images.removeChild(images.firstChild);
+	}
+}
+
+function onLoad() {
+	document.getElementById('calculate-button').addEventListener('click', callImgGen);
+	document.getElementById('save-button').addEventListener('click', saveImage);
+	document.getElementById('clear-button').addEventListener('click', clearButton);
+	document.getElementById('manufacturer-select').addEventListener('change', populateDeviceDropdown);
+	document.getElementById('screen-cols').addEventListener('input', drawGrid);
+	document.getElementById('screen-rows').addEventListener('input', drawGrid);
+	deviceDropdown.addEventListener('change', updateDeviceInfo);
+	manufacturerDropdown.addEventListener('change', populateDeviceDropdown);	
+
+	readManufacturers();
+}
